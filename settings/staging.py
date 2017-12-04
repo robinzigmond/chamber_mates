3f1a8e0369686164be5e35a2adac385cf91c1d80
Robin Zigmond <robinzig@hotmail.com>
2017-12-04 22:50:28 +0000

    added extra lines to settings/staging to allow Django to find the geospatial libraries

--- settings/staging.py
+++ settings/staging.py
@@ -22,3 +22,6 @@ LOGGING = {
 
 DATABASES["default"] = dj_database_url.config()
 DATABASES["default"]["ENGINE"] = "django.contrib.gis.db.backends.postgis"
+
+GDAL_LIBRARY_PATH = os.getenv("GDAL_LIBRARY_PATH")
+GEOS_LIBRARY_PATH = os.getenv("GEOS_LIBRARY_PATH")
